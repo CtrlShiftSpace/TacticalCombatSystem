dde54dfe62b35630276634fe26209920c5426e6a
luke <lukeforenergy@gmail.com>
2025-09-08 17:49:19 +0800

    Add SpawnGridInstance function to create grid space

--- Source/TacticalCombatSystem/Private/Actor/GridActor/TactCombGridInstActor.cpp
+++ Source/TacticalCombatSystem/Private/Actor/GridActor/TactCombGridInstActor.cpp
@@ -15,3 +15,23 @@ void ATactCombGridInstActor::BeginPlay()
 {
 	Super::BeginPlay();
 }
+
+void ATactCombGridInstActor::SpawnGridInstance(const FGridInstanceParam& InGridInstParam)
+{
+	const float ScaleDist = 0.5f;
+	// 計算網格總大小的一半
+	const FVector2D HalfGridSizeXY = FVector2D(InGridInstParam.WidthGridNum ,InGridInstParam.LengthGridNum) * FVector2D(InGridInstParam.GridSize.X, InGridInstParam.GridSize.Y) / 2;
+	// 計算左下角的座標
+	const FVector LeftBottomCornerLocation = InGridInstParam.CenterLocation - FVector(HalfGridSizeXY.X, HalfGridSizeXY.Y, 0.f);
+
+	// 從左下位置依序建立網格
+	for (int32 X = 0; X < InGridInstParam.WidthGridNum; ++X)
+	{
+		for (int32 Y = 0; Y < InGridInstParam.LengthGridNum; ++Y)
+		{
+			const FVector InstanceLocation = LeftBottomCornerLocation + FVector(InGridInstParam.GridSize.X * (X + ScaleDist), InGridInstParam.GridSize.Y * (Y + ScaleDist), 0.f);
+			const FTransform InstanceTransform = FTransform(FRotator::ZeroRotator, InstanceLocation, FVector(InGridInstParam.GridSize.X / 100.f, InGridInstParam.GridSize.Y / 100.f, 1.f));
+			GridInstMesh->AddInstance(InstanceTransform);
+		}
+	}
+}
--- Source/TacticalCombatSystem/Public/Actor/GridActor/TactCombGridInstActor.h
+++ Source/TacticalCombatSystem/Public/Actor/GridActor/TactCombGridInstActor.h
@@ -6,6 +6,30 @@
 #include "Actor/GridActor/TactCombGridActorBase.h"
 #include "TactCombGridInstActor.generated.h"
 
+// 網格 Instance 參數設定
+USTRUCT(BlueprintType)
+struct FGridInstanceParam
+{
+	GENERATED_BODY()
+
+	// 網格的中心位置
+	UPROPERTY(EditDefaultsOnly)
+	FVector CenterLocation = FVector::ZeroVector;
+
+	// 網格大小
+	UPROPERTY(EditDefaultsOnly)
+	FVector GridSize = FVector(100.f, 100.f, 1.f);
+	
+	// 長邊的網格數量
+	UPROPERTY(EditDefaultsOnly)
+	int32 LengthGridNum = 5;
+
+	// 寬邊的網格數量
+	UPROPERTY(EditDefaultsOnly)
+	int32 WidthGridNum = 4;
+	
+};
+
 /**
  * 
  */
@@ -19,9 +43,16 @@ public:
 	
 protected:
 	virtual void BeginPlay() override;
-
+	
+	// 產生網格 Instance
+	UFUNCTION(BlueprintCallable, Category = "GridInst Process")
+	void SpawnGridInstance(const FGridInstanceParam& InGridInstParam);
+	
 	// 靜態 Instance 物件
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	TObjectPtr<UInstancedStaticMeshComponent> GridInstMesh;
+
+	UPROPERTY(EditDefaultsOnly, Category = "GridInst Settings")
+	FGridInstanceParam GridInstParam;
 	
 };
