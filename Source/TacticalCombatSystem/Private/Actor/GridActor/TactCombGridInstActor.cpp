e5fb3990d92fe61bca5711ea44d2723df14eb9f2
luke <lukeforenergy@gmail.com>
2025-09-12 18:04:03 +0800

    Add FGridInstanceTile struct and remove FGridInstanceParam struct

--- Source/TacticalCombatSystem/Private/Actor/GridActor/TactCombGridInstActor.cpp
+++ Source/TacticalCombatSystem/Private/Actor/GridActor/TactCombGridInstActor.cpp
@@ -13,6 +13,60 @@ ATactCombGridInstActor::ATactCombGridInstActor()
 	GridInstMesh->SetupAttachment(RootComponent);
 }
 
+TArray<FGridInstanceTile> ATactCombGridInstActor::GetGridInstTiles(const UGridClassInfo* GridClassInfo)
+{
+	LeftBottomCornerLocation = GetLeftBottomCornerLocation();
+	const FGridClassAssetInfo& GridAssetInfo = GridClassInfo->GetGridClassAssetInfo(GridShape);
+	// 計算網格的縮放比例
+	const FVector GridScale = GetGridScale(GridClassInfo) * GridSize / GridAssetInfo.MeshSize;
+	const FVector2D GridTileSize = GetGridTileSize();
+	TArray<FGridInstanceTile> GridInstTilesRecord;
+	
+	// 從左下位置依序建立網格
+	for (int32 X = 0; X < LengthGridNum; ++X)
+	{
+		// 計算額外要增加的偏移量
+		FVector InstanceOffset = FVector::ZeroVector;
+		// X 是否為奇數
+		const bool bXOdd = (FMath::Modulo(X, 2) == 1);
+		// 如果是奇數列且為六邊形
+		if (bXOdd && GridShape == EGridShape::Hexagon)
+		{
+			// 需要半個格子的偏移
+			InstanceOffset.Y = GridTileSize.Y * 0.5f;
+		}
+
+		for (int32 Y = 0; Y < WidthGridNum; ++Y)
+		{
+			FRotator InstanceRotator = FRotator::ZeroRotator;
+			// Y 是否為奇數
+			const bool bYOdd = (FMath::Modulo(Y, 2) == 1);
+			// 三角形的判斷
+			if (GridShape == EGridShape::Triangle)
+			{
+				// 只有 X 與 Y 其中一個是奇數，才旋轉 180 度
+				if (bXOdd != bYOdd)
+				{
+					InstanceRotator = FRotator(0.f, 180.f, 0.f);
+				}
+			}
+
+			// 最左下座標 + (X * 格子長度, Y * 格子寬度) + 偏移量 + 額外偏移量
+			const FVector InstanceLocation = LeftBottomCornerLocation +
+												FVector(GridTileSize.X * X, GridTileSize.Y * Y, 0.f) +
+												InstanceOffset + TileOffsetVector;
+			FGridInstanceTile GridInstTile;
+			GridInstTile.TileTransform = FTransform(
+				InstanceRotator,
+				InstanceLocation,
+				GridScale
+			);
+			GridInstTilesRecord.Add(GridInstTile);
+		}
+	}
+	return GridInstTilesRecord;
+}
+
 void ATactCombGridInstActor::BeginPlay()
 {
 	Super::BeginPlay();
@@ -23,65 +77,51 @@ void ATactCombGridInstActor::BeginPlay()
 
 void ATactCombGridInstActor::SpawnGridInstance(const UGridClassInfo* GridClassInfo)
 {
-	GridInstParam.LeftBottomCornerLocation = GetLeftBottomCornerLocation();
 	// 依照設定的形狀取得網格的素材
 	const FGridClassAssetInfo& GridAssetInfo = GridClassInfo->GetGridClassAssetInfo(GridShape);
-	// 計算網格的縮放比例
-	const FVector GridScale = GetGridScale(GridClassInfo);
-	const FVector2D GridTileSize = GetGridTileSize();
-	// 取得網格平面左下角的座標位置
-	const FVector& LeftBottomCornerLocation = GridInstParam.LeftBottomCornerLocation;
 	// 移除現有的 Instance 資訊
 	GridInstMesh->ClearInstances();
 	// 使用 Border 的 Mesh
 	UStaticMesh* InstanceMesh = GridAssetInfo.GridFlatMesh;
 	GridInstMesh->SetStaticMesh(InstanceMesh);
 	GridInstMesh->SetMaterial(0, GridAssetInfo.GridFlatBorderMaterial);
-	
-	// 從左下位置依序建立網格
-	for (int32 X = 0; X < GridInstParam.LengthGridNum; ++X)
-	{
-		FVector InstanceOffset = FVector::ZeroVector;
-		// 六邊形的判斷
-		if (GridShape == EGridShape::Hexagon)
-		{
-			// 如果是奇數列，則需要半個格子的偏移
-			if (FMath::Modulo(X, 2) == 1)
-			{
-				InstanceOffset.Y = GridTileSize.Y * 0.5f;
-			}
-		}
 
-		for (int32 Y = 0; Y < GridInstParam.WidthGridNum; ++Y)
-		{
-			FRotator InstanceRotator = FRotator::ZeroRotator;
-			// 三角形的判斷
-			if (GridShape == EGridShape::Triangle)
-			{
-				bool bXOdd = (FMath::Modulo(X, 2) == 1);
-                bool bYOdd = (FMath::Modulo(Y, 2) == 1);
-				// 只有 X 與 Y 其中一個是奇數，才旋轉 180 度
-				if (bXOdd != bYOdd)
-				{
-					InstanceRotator = FRotator(0.f, 180.f, 0.f);
-				}
-			}
-			
-			const FVector InstanceLocation = LeftBottomCornerLocation + FVector(GridTileSize.X * X, GridTileSize.Y * Y, 0.f) + InstanceOffset;
-			const FTransform InstanceTransform = FTransform(
-				InstanceRotator,
-				InstanceLocation,
-				GridScale
-			);
-			GridInstMesh->AddInstance(InstanceTransform);
-		}
+	// 長或寬小於等於 0 無法生成網格
+	if (LengthGridNum <= 0 || WidthGridNum <= 0)
+	{
+		return;
 	}
+
+	// 取得要生成的網格資訊
+	GridInstTiles = GetGridInstTiles(GridClassInfo);
+	// 依序加入 Instance
+	for (const FGridInstanceTile& GridInstTile : GridInstTiles)
+	{
+		GridInstMesh->AddInstance(GridInstTile.TileTransform);
+	}
+
+	// // 測試偵測線段
+	// FVector DetectGroundStart = InstanceLocation + FVector(0.f, 0.f, 500.f);
+	// FVector DetectGroundEnd = InstanceLocation + FVector(0.f, 0.f, -500.f);
+	// // 轉換到世界座標
+	// FVector DetectGroundInWorldStart = GetActorTransform().TransformPosition(DetectGroundStart);
+	// FVector DetectGroundInWorldEnd = GetActorTransform().TransformPosition(DetectGroundEnd);
+	// 		
+	// const TArray<AActor*>& ActorsToIgnore = TArray<AActor*>();
+	// FHitResult HitResult;
+	// const bool bDetectGround = UKismetSystemLibrary::LineTraceSingle(this, DetectGroundInWorldStart, DetectGroundInWorldEnd, UEngineTypes::ConvertToTraceType(ECC_Visibility), false, ActorsToIgnore, EDrawDebugTrace::None, HitResult, true);
+	// if (bDetectGround)
+	// {
+	// 	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("Hit Ground Location: %s"), *HitResult.ImpactPoint.ToString()));
+	// }
+	// 		
+	// DrawDebugLine(GetWorld(), DetectGroundInWorldStart, DetectGroundInWorldEnd, FColor::Green, true, -1.f, 0, 5.f);
 }
 
 FVector2D ATactCombGridInstActor::GetGridTileSize() const
 {
 	// 計算網格平面大小，只取XY值
-	FVector2D GridTileSize = FVector2D(GridInstParam.GridSize);
+	FVector2D GridTileSize = FVector2D(GridSize);
 	if (GridShape == EGridShape::Triangle)
 	{
 		// 三角形的 Y 軸長度較短需要除以 2
@@ -97,14 +137,14 @@ FVector2D ATactCombGridInstActor::GetGridTileSize() const
 FVector2D ATactCombGridInstActor::GetGridPlaneSize() const
 {
 	const FVector2D GridTileSize = GetGridTileSize();
-	return FVector2D(GridInstParam.LengthGridNum, GridInstParam.WidthGridNum) * GridTileSize;
+	return FVector2D(LengthGridNum, WidthGridNum) * GridTileSize;
 }
 
 FVector ATactCombGridInstActor::GetLeftBottomCornerLocation() const
 {
 	const FVector2D HalfGridPlaneSize = FVector2D(
-		GridInstParam.LengthGridNum - 1 > 0 ? (GetGridTileSize().X * (GridInstParam.LengthGridNum - 1)) : 0.f,
-		GridInstParam.WidthGridNum - 1 > 0 ? (GetGridTileSize().Y * (GridInstParam.WidthGridNum - 1)) : 0.f
+		LengthGridNum - 1 > 0 ? (GetGridTileSize().X * (LengthGridNum - 1)) : 0.f,
+		WidthGridNum - 1 > 0 ? (GetGridTileSize().Y * (WidthGridNum - 1)) : 0.f
 	) * 0.5f;
-	return GridInstParam.CenterLocation - FVector(HalfGridPlaneSize.X, HalfGridPlaneSize.Y, 0.f);
+	return CenterLocation - FVector(HalfGridPlaneSize.X, HalfGridPlaneSize.Y, 0.f);
 }
--- Source/TacticalCombatSystem/Public/Actor/GridActor/TactCombGridInstActor.h
+++ Source/TacticalCombatSystem/Public/Actor/GridActor/TactCombGridInstActor.h
@@ -6,31 +6,17 @@
 #include "Actor/GridActor/TactCombGridActorBase.h"
 #include "TactCombGridInstActor.generated.h"
 
-// 網格 Instance 參數設定
+class UBoxComponent;
+
 USTRUCT(BlueprintType)
-struct FGridInstanceParam
+struct FGridInstanceTile
 {
 	GENERATED_BODY()
 
-	// 網格的中心位置
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	FVector CenterLocation = FVector::ZeroVector;
-
-	// 網格的左下角位置
+	// 網格位置
 	UPROPERTY()
-	FVector LeftBottomCornerLocation = FVector::ZeroVector;
+	FTransform TileTransform = FTransform(FRotator::ZeroRotator, FVector::ZeroVector, FVector::OneVector);
 
-	// 網格大小
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	FVector GridSize = FVector(100.f, 100.f, 100.f);
-	
-	// 長邊的網格數量
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	int32 LengthGridNum = 5;
-
-	// 寬邊的網格數量
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	int32 WidthGridNum = 4;
 	
 };
 
@@ -44,6 +30,10 @@ class TACTICALCOMBATSYSTEM_API ATactCombGridInstActor : public ATactCombGridActo
 	
 public:
 	ATactCombGridInstActor();
+
+	// 取得網格 Instance Tile
+	UFUNCTION()
+	TArray<FGridInstanceTile> GetGridInstTiles(const UGridClassInfo* GridClassInfo);
 	
 protected:
 	virtual void BeginPlay() override;
@@ -65,7 +55,32 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	TObjectPtr<UInstancedStaticMeshComponent> GridInstMesh;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "GridInst Settings")
-	FGridInstanceParam GridInstParam;
+	// 網格大小
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FVector GridSize = FVector(100.f, 100.f, 100.f);
 	
+	// 長邊的網格數量
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 LengthGridNum = 5;
+
+	// 寬邊的網格數量
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int32 WidthGridNum = 4;
+
+private:
+	// 網格 Instance 的自訂偏移向量，用於浮在地面上，避免同樣高度的物件 Z-Fighting
+	UPROPERTY()
+	FVector TileOffsetVector = FVector(0.f, 0.f, 2.f);
+
+	// 紀錄每個網格 Instance 的資料
+	UPROPERTY()
+	TArray<FGridInstanceTile> GridInstTiles;
+
+	// 網格的中心位置
+	UPROPERTY()
+	FVector CenterLocation = FVector::ZeroVector;
+
+	// 網格的左下角位置
+	UPROPERTY()
+	FVector LeftBottomCornerLocation = FVector::ZeroVector;
 };
